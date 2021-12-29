f5ddf2f0477d88650aceab15630523effe659621
Nathan Olinto <nathanolinto@gmail.com>
2021-12-29 13:35:56 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,15 @@
+import 'dotenv/config';
+import cors from 'cors';
+import express from 'express';
+
+import { router } from './routes';
+
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+
+app.use(router);
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`🏃 Server is running on PORT ${port}`));
