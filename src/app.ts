605f52bc640164010f8b51c8c050d4fb9c25c365
nathanolinto <nathanolinto@gmail.com>
2021-12-31 09:30:43 -0300

    init Documentation swagger

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import "dotenv/config";
 import cors from "cors";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
 
 const port = process.env.PORT || 4000;
