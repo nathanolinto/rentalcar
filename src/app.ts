aca0ed665041b280abc0a0ae847d72aa7a9a750f
nathanolinto <nathanolinto@gmail.com>
2022-01-03 11:34:36 -0300

    Configurando o Docker, app e o database postgres

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,8 @@ import swaggerUi from "swagger-ui-express";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
+import "./database";
+
 const app = express();
 app.use(cors());
 app.use(express.json());
